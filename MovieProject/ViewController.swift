d4922906f7aed702b5e56906acc68d5e7ce73344
Dave <daveharry.ios@gmail.com>
2020-06-12 08:39:44 +0700

    Initial Commit

---
+++ MovieProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieProject
+//
+//  Created by Dave Harry on 12/06/20.
+//  Copyright © 2020 Dave Harry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
